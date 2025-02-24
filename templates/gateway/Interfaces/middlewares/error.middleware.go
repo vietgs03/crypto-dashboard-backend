nguyenhoang711/crypto-dashboard-backend-hoangndh

config more project in 20/2/2025

templates/gateway/Interfaces/middlewares/error.middleware.go
@@ -1,0 +1,26 @@
+package middlewares
+
+import (
+	"reflect"
+
+	"crypto-dashboard/gw-example/infrastructure/core"
+	"crypto-dashboard/pkg/common"
+	"crypto-dashboard/pkg/constants"
+	"crypto-dashboard/pkg/response"
+
+	"github.com/gofiber/fiber/v3"
+)
+
+// TODO if Error status 500 to send notfi discord
+func ErrorHandler(c fiber.Ctx, err error) error {
+	if err != nil && !reflect.ValueOf(err).IsNil() {
+		internalError := response.NewAppError(constants.InternalServerErr, err.Error())
+		if appErr, ok := err.(*response.AppError); ok {
+			internalError = appErr
+		}
+
+		c.Status(constants.HttpCode[internalError.Code]).JSON(common.ErrorResponse(core.GetHttpReqCtx(c), internalError))
+	}
+
+	return nil
+}

templates/gateway/infrastructure/core/http_ctx.helper.go
@@ -1,0 +1,21 @@
+package core
+
+import (
+	"crypto-dashboard/pkg/common"
+	"crypto-dashboard/pkg/constants"
+	"crypto-dashboard/pkg/dtos/duser"
+
+	"github.com/gofiber/fiber/v3"
+)
+
+func GetHttpUserCtx(c fiber.Ctx) *duser.UserInfo[any] {
+	return c.Locals(constants.REQUEST_CONTEXT_KEY).(*common.ReqContext).UserInfo
+}
+
+func GetHttpReqCtx(c fiber.Ctx) *common.ReqContext {
+	return c.Locals(constants.REQUEST_CONTEXT_KEY).(*common.ReqContext)
+}
+
+func SetHttpUserCtx(c fiber.Ctx, user *duser.UserInfo[any]) {
+	c.Locals(constants.REQUEST_CONTEXT_KEY).(*common.ReqContext).UserInfo = user
+}

vietgs03/crypto-dashboard-backend

templates/gateway/Interfaces/middlewares/error.middleware.go
@@ -1,0 +1,26 @@
+package middlewares
+
+import (
+	"reflect"
+
+	"crypto-dashboard/gw-example/infrastructure/helper"
+	"crypto-dashboard/pkg/common"
+	"crypto-dashboard/pkg/constants"
+	"crypto-dashboard/pkg/response"
+
+	"github.com/gofiber/fiber/v3"
+)
+
+// TODO if Error status 500 to send notfi discord
+func ErrorHandler(c fiber.Ctx, err error) error {
+	if err != nil && !reflect.ValueOf(err).IsNil() {
+		internalError := response.NewAppError(constants.InternalServerErr, err.Error())
+		if appErr, ok := err.(*response.AppError); ok {
+			internalError = appErr
+		}
+
+		c.Status(constants.HttpCode[internalError.Code]).JSON(common.ErrorResponse(helper.GetHttpReqCtx(c), internalError))
+	}
+
+	return nil
+}

templates/gateway/infrastructure/helper/http_ctx.helper.go
@@ -1,0 +1,21 @@
+package helper
+
+import (
+	"crypto-dashboard/pkg/common"
+	"crypto-dashboard/pkg/constants"
+	"crypto-dashboard/pkg/dtos/duser"
+
+	"github.com/gofiber/fiber/v3"
+)
+
+func GetHttpUserCtx(c fiber.Ctx) *duser.UserInfo[any] {
+	return c.Locals(constants.REQUEST_CONTEXT_KEY).(*common.ReqContext).UserInfo
+}
+
+func GetHttpReqCtx(c fiber.Ctx) *common.ReqContext {
+	return c.Locals(constants.REQUEST_CONTEXT_KEY).(*common.ReqContext)
+}
+
+func SetHttpUserCtx(c fiber.Ctx, user *duser.UserInfo[any]) {
+	c.Locals(constants.REQUEST_CONTEXT_KEY).(*common.ReqContext).UserInfo = user
+}